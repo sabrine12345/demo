455c3ff5ec57daa9080295b2e8a6a7bb06448f98
sabdelkabir <sabdelkabir@talys.digital>
2025-10-22 12:15:45 +0100

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
+// main class
 	public static void main(String[] args) {
 		
 		SpringApplication.run(DemoApplication.class, args);
