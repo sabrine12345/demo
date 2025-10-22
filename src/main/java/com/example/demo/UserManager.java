cffd97a8085892f4d5234abe38f02a3587297833
sabdelkabir <sabdelkabir@talys.digital>
2025-10-22 12:26:25 +0100

    code review

--- src/main/java/com/example/demo/UserManager.java
+++ src/main/java/com/example/demo/UserManager.java
@@ -1,16 +1,37 @@
-package com.example.demo;
+package com.example.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.*;
 
-@SpringBootApplication
-public class DemoApplication {
+public class UserManager {
+    private List<String> users = new ArrayList<>();
+    private Map<String, Integer> loginAttempts = new HashMap<>();
 
+    public void addUser(String username) {
+        if (!users.contains(username)) {
+            users.add(username);
+        } else {
+            System.out.println("User already exists!");
+        }
+    }
 
-// main class
-	public static void main(String[] args) {
-		
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("coucou");
-	}
+    public boolean login(String username, String password) {
+        if (password.equals("1234")) {
+            System.out.println("Logged in!");
+            return true;
+        } else {
+            int attempts = loginAttempts.getOrDefault(username, 0);
+            attempts++;
+            loginAttempts.put(username, attempts);
+            if (attempts > 3) {
+                System.out.println("Too many attempts. Account locked!");
+            }
+            return false;
+        }
+    }
+
+    public void showUsers() {
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println("User: " + users.get(i));
+        }
+    }
 }
